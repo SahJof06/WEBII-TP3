37cfa9c40589129669e29de1b51aeb1d77092b7a
SahJof06 <142264032+SahJof06@users.noreply.github.com>
2024-05-06 14:08:28 -0300

    Add files via upload

--- extra2.js
+++ extra2.js
@@ -1,8 +1,18 @@
- var s = 20
- var d = 25
- var t = 28
+var s = 20
+var d = 25
+var t = 28
 
- var cantidad_1 = parseInt(prompt("Cuantas hamburguesas simples quiere comprar"));
- var cantidad_2 = parseInt(prompt("Cuantas hamburguesas dobles quiere comprar"));
- var cantidad_3 = parseInt(prompt("Cuantas hamburguesas tercer quiere comprar"));
- 
+var cantidad_1 = parseInt(prompt("Cuantas hamburguesas simples quiere comprar"));
+var cantidad_2 = parseInt(prompt("Cuantas hamburguesas dobles quiere comprar"));
+var cantidad_3 = parseInt(prompt("Cuantas hamburguesas triples quiere comprar"));
+var pago = prompt("Ingrese el metodo de pago (tarjeta/efectivo)");
+
+if (pago == "tarjeta") {
+    let total = (cantidad_1 * s) + (cantidad_2 * d) + (cantidad_3 * t);
+    let cargo = (total * 0.05) + total;
+    alert("El costo total es de: $" + cargo);
+} else {
+    let total = (cantidad_1 * s) + (cantidad_2 * d) + (cantidad_3 * t);
+
+    alert("El costo total es de: " + total);
+}
---
+++ extra3.js
@@ -0,0 +1,5 @@
+var cant = parseInt(prompt("Ingrese la cantidad de productos que llevará"));
+for(i=1;i<=cant;i++){
+    var valor = parseInt(prompt("Ingrese el valor del producto"));
+    const acum = valor;
+}
